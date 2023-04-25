8ab34af3f66170538ee81a84add18a950e464f61
Mona Lisa <pedromgpinela@gmail.com>
2023-04-25 21:37:32 +0100

    first commit

---
+++ starter/Person.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Person = ({ id, name, age, image }) => {
+  return (
+    <article className="person">
+      <img src={image} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years old</p>
+      </div>
+    </article>
+  );
+};
+
+export default Person;
---
+++ starter/src/App.jsx
@@ -0,0 +1,22 @@
+import data from "../src/data";
+import { useState } from "react";
+import List from "../List";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  const clearAll = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button type="button" className="btn btn-block" onClick={clearAll}>
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
